2f14c7660bbee268ff2afdac68a5138a9ec5b29c
Richies Huynh <richies2006huynh@gmail.com>
2025-06-22 03:59:49 -0700

    Loops text on LCD screen

--- Services/LCDService.py
+++ Services/LCDService.py
@@ -2,17 +2,29 @@ from RPLCD.i2c import CharLCD
 from time import sleep
 
 class LCDService:
-    def display(self, message = "Hello World", delay = 0.3, loop = True):
+    def display(self, message="Hello World", delay=0.3):
+        """Display message on LCD with proper scrolling."""
+        if not self.is_ready():
+            print("LCD not ready")
+            return
+        
         self.lcd = CharLCD('PCF8574', 0x27)
         self.lcd.clear()
         self.lcd.write_string(message)
         self.width = 16
 
+        # Always run through the message once (do-while behavior)
         while True:
+            # Scroll from left to right (beginning to end)
             for i in range(len(message) - self.width + 1):
                 self.lcd.clear()
                 self.lcd.write_string(message[i:i + self.width])
                 sleep(delay)
             
-            if not loop:
-                break
+            # If message is longer than display width, scroll back to beginning
+            if len(message) > self.width:
+                # Scroll from right to left (end to beginning)
+                for i in range(len(message) - self.width, -1, -1):
+                    self.lcd.clear()
+                    self.lcd.write_string(message[i:i + self.width])
+                    sleep(delay)
