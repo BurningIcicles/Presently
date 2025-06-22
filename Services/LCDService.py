9df358d48f17454f04dea3269b6985164a97b59c
Richies Huynh <richies2006huynh@gmail.com>
2025-06-22 04:01:29 -0700

    Removed check

--- Services/LCDService.py
+++ Services/LCDService.py
@@ -4,10 +4,6 @@ from time import sleep
 class LCDService:
     def display(self, message="Hello World", delay=0.3):
         """Display message on LCD with proper scrolling."""
-        if not self.is_ready():
-            print("LCD not ready")
-            return
-        
         self.lcd = CharLCD('PCF8574', 0x27)
         self.lcd.clear()
         self.lcd.write_string(message)
