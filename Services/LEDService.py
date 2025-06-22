590f3496e1d7fb55e01b29fea3b90b518e32a622
Richies Huynh <richies2006huynh@gmail.com>
2025-06-22 04:35:27 -0700

    Adjusted pin on breadboard

--- Services/LEDService.py
+++ Services/LEDService.py
@@ -3,9 +3,9 @@ import time
 
 class LEDService:
     def light(self, blink_count):
-        self.pin = 17 #GPIO17 = Pin 11
+        self.pin = 4 #GPIO4 = Pin 7
         GPIO.setmode(GPIO.BCM)
-        GPIO.setup(self.pin, GPIO.OUT)  # GPIO17 = Pin 11
+        GPIO.setup(self.pin, GPIO.OUT)  # GPIO17 = Pin 7
         for i in range(blink_count):
             GPIO.output(17, GPIO.HIGH)  # LED ON
             time.sleep(0.5)
