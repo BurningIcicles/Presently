19f44c49c7e024dc90808a44d0daeb79e7d86ccd
Richies Huynh <richies2006huynh@gmail.com>
2025-06-22 02:43:39 -0700

    Added LCDService.py

---
+++ Controllers/LCDController.py
@@ -0,0 +1,11 @@
+# CameraController is used to run the camera service functions
+import sys
+import os
+
+# Add the parent directory to the path so we can import from sibling directories
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+from Services.LCDService import LCDService
+
+if __name__ == "__main__":
+    LCDService = LCDService()
+    LCDService.display("freakyyy")
