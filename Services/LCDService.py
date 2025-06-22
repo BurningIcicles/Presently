7d4a5fddf0d60505bf13c54e0431df4039b9428d
Richies Huynh <richies2006huynh@gmail.com>
2025-06-22 04:08:11 -0700

    Adjusted delay again

--- Services/LCDService.py
+++ Services/LCDService.py
@@ -9,14 +9,14 @@ class LCDService:
         self.lcd.write_string(message)
         self.width = 16
 
-        sleep(delay * 3)
+        sleep(delay * 10)
         # Always run through the message once (do-while behavior)
         while True:
             # Scroll from left to right (beginning to end)
             for i in range(len(message) - self.width + 1):
                 self.lcd.clear()
                 self.lcd.write_string(message[i:i + self.width])
-                if (i == len(message) - self.width + 1):
-                    sleep(delay * 3)
+                if i == len(message) - self.width:
+                    sleep(delay * 10)
                 else:
                     sleep(delay)
